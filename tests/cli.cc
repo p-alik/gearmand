0f31f37f4085c6f94874766887e103301ef2f7c3
Brian Aker <brian@tangent.org>
2011-10-27 23:37:08 -0700

    Merge in fixes, disable admin for check.

--- tests/cli.cc
+++ tests/cli.cc
@@ -59,7 +59,7 @@ static std::string executable;
 
 #define WORKER_FUNCTION_NAME "echo_function"
 
-static test_return_t help_test(void *)
+static test_return_t gearman_help_test(void *)
 {
   const char *args[]= { "-H", 0 };
 
@@ -67,7 +67,7 @@ static test_return_t help_test(void *)
   return TEST_SUCCESS;
 }
 
-static test_return_t unknown_test(void *)
+static test_return_t gearman_unknown_test(void *)
 {
   char buffer[1024];
   snprintf(buffer, sizeof(buffer), "-p %d", int(default_port()));
@@ -79,7 +79,7 @@ static test_return_t unknown_test(void *)
   return TEST_SUCCESS;
 }
 
-static test_return_t client_background_test(void *)
+static test_return_t gearman_client_background_test(void *)
 {
   char buffer[1024];
   snprintf(buffer, sizeof(buffer), "-p %d", int(default_port()));
@@ -110,17 +110,149 @@ static test_return_t regression_833394_test(void *)
   return TEST_SUCCESS;
 }
 
+static test_return_t gearadmin_help_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--help", 0 };
+
+  test_true(exec_cmdline(executable, args));
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_shutdown_test(void *object)
+{
+  server_startup_st *servers= (server_startup_st *)object;
+
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--shutdown", 0 };
+
+  test_true(exec_cmdline(executable, args));
+
+  Server *server= servers->pop_server();
+  test_true(server);
+  while (server->ping()) 
+  {
+    // Wait out the death of the server
+  }
+
+  // Since we killed the server above, we need to reset it
+  server->reset();
+  delete server;
+
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_version_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--server-version", 0 };
+
+  test_true(exec_cmdline(executable, args));
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_verbose_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--server-verbose", 0 };
+
+  test_true(exec_cmdline(executable, args));
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_status_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--status", 0 };
+
+  test_true(exec_cmdline(executable, args));
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_workers_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--workers", 0 };
+
+  test_true(exec_cmdline(executable, args));
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_create_drop_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+
+  const char *create_args[]= { buffer, "--create-function=test_function", 0 };
+  test_true(exec_cmdline(executable, create_args));
+
+  const char *drop_args[]= { buffer, "--drop-function=test_function", 0 };
+  test_true(exec_cmdline(executable, drop_args));
+
+
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_getpid_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--getpid", 0 };
+
+  test_true(exec_cmdline(executable, args));
+  return TEST_SUCCESS;
+}
+
+static test_return_t gearadmin_unknown_test(void *)
+{
+  char buffer[1024];
+  snprintf(buffer, sizeof(buffer), "--port=%d", int(default_port()));
+  const char *args[]= { buffer, "--unknown", 0 };
+
+  // The argument doesn't exist, so we should see an error
+  test_false(exec_cmdline(executable, args));
+
+  return TEST_SUCCESS;
+}
+
+
 test_st gearman_tests[] ={
-  { "--help", 0, help_test },
-  { "-H", 0, help_test },
-  { "--unknown", 0, unknown_test },
-  { "-f echo -b payload", 0, client_background_test },
+  { "--help", 0, gearman_help_test },
+  { "-H", 0, gearman_help_test },
+  { "--unknown", 0, gearman_unknown_test },
+  { "-f echo -b payload", 0, gearman_client_background_test },
   { "lp:833394", 0, regression_833394_test },
   { 0, 0, 0 }
 };
 
+
+test_st gearadmin_tests[] ={
+  {"--help", 0, gearadmin_help_test},
+  {"--server-version", 0, gearadmin_version_test},
+  {"--server-verbose", 0, gearadmin_verbose_test},
+  {"--status", 0, gearadmin_status_test},
+  {"--getpid", 0, gearadmin_getpid_test},
+  {"--workers", 0, gearadmin_workers_test},
+  {"--create-function and --drop-function", 0, gearadmin_create_drop_test},
+  {"--unknown", 0, gearadmin_unknown_test},
+  {"--shutdown", 0, gearadmin_shutdown_test}, // Must be run last since it shuts down the server
+  {0, 0, 0}
+};
+
+static test_return_t pre_skip(void*)
+{
+  return TEST_SKIPPED;
+}
+
 collection_st collection[] ={
   {"gearman", 0, 0, gearman_tests},
+  {"gearadmin", pre_skip, 0, gearadmin_tests},
   {0, 0, 0, 0}
 };
 
