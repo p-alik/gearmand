11779291fd16589a343b047a5449def5118c06c4
Brian Aker <brian@tangent.org>
2011-12-31 17:37:03 -0800

    Fire all guns,...

--- libtest/http.cc
+++ libtest/http.cc
@@ -45,13 +45,17 @@ extern "C" size_t
 
 static void init(CURL *curl, const std::string& url)
 {
+#if defined(HAVE_LIBCURL) && HAVE_LIBCURL
+  if (HAVE_LIBCURL)
   assert(curl);
   curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
   curl_easy_setopt(curl, CURLOPT_USERAGENT, YATL_USERAGENT);
+#endif
 }
 
 bool GET::execute()
 {
+#if defined(HAVE_LIBCURL) && HAVE_LIBCURL
   if (HAVE_LIBCURL)
   {
     CURL *curl= curl_easy_init();
@@ -68,12 +72,14 @@ bool GET::execute()
 
     return retref == CURLE_OK;
   }
+#endif
 
   return false;
 }
 
 bool POST::execute()
 {
+#if defined(HAVE_LIBCURL) && HAVE_LIBCURL
   if (HAVE_LIBCURL)
   {
     CURL *curl= curl_easy_init();;
@@ -88,11 +94,14 @@ bool POST::execute()
 
     curl_easy_cleanup(curl);
   }
+#endif
+
   return false;
 }
 
 bool TRACE::execute()
 {
+#if defined(HAVE_LIBCURL) && HAVE_LIBCURL
   if (HAVE_LIBCURL)
   {
     CURL *curl= curl_easy_init();;
@@ -110,12 +119,14 @@ bool TRACE::execute()
 
     return retref == CURLE_OK;
   }
+#endif
 
   return false;
 }
 
 bool HEAD::execute()
 {
+#if defined(HAVE_LIBCURL) && HAVE_LIBCURL
   if (HAVE_LIBCURL)
   {
     CURL *curl= curl_easy_init();;
@@ -132,6 +143,7 @@ bool HEAD::execute()
 
     return retref == CURLE_OK;
   }
+#endif
 
   return false;
 }
