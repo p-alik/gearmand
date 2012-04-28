21df0f3853bab89a26eba99dac05187f8e8c5fbe
Brian Aker <brian@tangent.org>
2012-04-28 01:01:43 -0700

    Update all licenses to BSD.

--- util/pidfile.cc
+++ util/pidfile.cc
@@ -35,14 +35,14 @@
  *
  */
 
-#include <config.h>
+#include "config.h"
 
 #include "util/pidfile.hpp"
 
-#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#include <cerrno>
 #include <fcntl.h>
 #include <iostream>
 #include <sstream>
@@ -118,14 +118,8 @@ bool Pidfile::create()
     }
   }
 
-  int oflags= O_CREAT|O_WRONLY|O_TRUNC;
-  if (HAVE_O_CLOEXEC)
-  {
-    oflags= oflags | O_CLOEXEC;
-  }
-
   int file;
-  if ((file= open(_filename.c_str(), oflags, S_IRWXU|S_IRGRP|S_IROTH)) < 0)
+  if ((file = open(_filename.c_str(), O_CREAT|O_WRONLY|O_TRUNC, S_IRWXU|S_IRGRP|S_IROTH)) < 0)
   {
     std::stringstream error_stream;
     error_stream << "Could not open pid file for writing: " << _filename << "(" << strerror(errno) << ")";
