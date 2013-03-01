a3488a05b39ac4f31bc2e715adf6301038888f1e
Brian Aker <brian@tangent.org>
2013-03-01 10:20:20 -0800

    Update style.

--- libgearman-server/client.cc
+++ libgearman-server/client.cc
@@ -78,44 +78,39 @@ gearman_server_client_add(gearman_server_con_st *con)
     return NULL;
   }
 
-  client->con= con;
-  GEARMAN_LIST_ADD(con->client, client, con_);
+  client->init(con);
 
-  client->job= NULL;
-  client->job_next= NULL;
-  client->job_prev= NULL;
+  GEARMAN_LIST_ADD(con->client, client, con_);
 
   return client;
 }
 
 void gearman_server_client_free(gearman_server_client_st *client)
 {
-  if (client == NULL)
+  if (client)
   {
-    return;
-  }
+    GEARMAN_LIST_DEL(client->con->client, client, con_);
 
-  GEARMAN_LIST_DEL(client->con->client, client, con_);
-
-  if (client->job)
-  {
-    GEARMAN_LIST_DEL(client->job->client, client, job_);
-
-    /* If this was a foreground job and is now abandoned, mark to not run. */
-    if (client->job->client_list == NULL)
+    if (client->job)
     {
-      client->job->ignore_job= true;
-      client->job->job_queued= false;
-    }
-  }
+      GEARMAN_LIST_DEL(client->job->client, client, job_);
 
-  if (Server->free_client_count < GEARMAN_MAX_FREE_SERVER_CLIENT)
-  {
-    GEARMAN_LIST_ADD(Server->free_client, client, con_)
-  }
-  else
-  {
-    gearmand_debug("delete gearman_server_client_st");
-    delete client;
+      /* If this was a foreground job and is now abandoned, mark to not run. */
+      if (client->job->client_list == NULL)
+      {
+        client->job->ignore_job= true;
+        client->job->job_queued= false;
+      }
+    }
+
+    if (Server->free_client_count < GEARMAN_MAX_FREE_SERVER_CLIENT)
+    {
+      GEARMAN_LIST_ADD(Server->free_client, client, con_)
+    }
+    else
+    {
+      gearmand_debug("delete gearman_server_client_st");
+      delete client;
+    }
   }
 }
