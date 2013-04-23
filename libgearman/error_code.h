96366448f8ba0992e0f6cce49fbd183e728714ac
Brian Aker <brian@tangent.org>
2013-04-23 07:46:38 -0700

    Adding new files.

--- libgearman/error_code.h
+++ libgearman/error_code.h
@@ -2,8 +2,7 @@
  * 
  *  Gearmand client and server library.
  *
- *  Copyright (C) 2011-2013 Data Differential, http://datadifferential.com/
- *  Copyright (C) 2008 Brian Aker, Eric Day
+ *  Copyright (C) 2013 Data Differential, http://datadifferential.com/
  *  All rights reserved.
  *
  *  Redistribution and use in source and binary forms, with or without
@@ -36,55 +35,8 @@
  *
  */
 
-
 #pragma once
 
-struct Client;
-struct Task;
-struct Worker;
-struct gearman_client_st;
-struct gearman_connection_st;
-struct gearman_result_st;
-struct gearman_universal_st;
+#include <libgearman-1.0/return.h>
 
-#include "libgearman-1.0/gearman.h"
-
-#include "libgearman/server_options.hpp"
-#include "libgearman/connection.hpp"
-#include "libgearman/universal.hpp"
-#include "libgearman/allocator.hpp"
-#include "libgearman/packet.hpp"
-#include "libgearman/run.hpp"
-#include "libgearman/aggregator.hpp"
-
-#include "libgearman/error.hpp"
-#include "libgearman/error_code.h"
-
-/* These are private not to be installed headers */
-#include "libgearman/byteorder.h"
-#include "libgearman/strcommand.h"
-#include "libgearman/vector.hpp"
-#include "libgearman/unique.hpp"
-#include "libgearman/add.hpp"
-#include "libgearman/is.hpp"
-#include "libgearman/result.hpp"
-#include "libgearman/actions.hpp"
-#include "libgearman/string.hpp"
-#include "libgearman/command.h"
-#include "libgearman/task.hpp"
-#include "libgearman/job.h"
-#include "libgearman/job.hpp"
-#include "libgearman/client.h"
-#include "libgearman/server_options.hpp"
-#include "libgearman/check.h"
-
-#include "libgearman/status.hpp"
-
-#include "libgearman/task_attr.h"
-
-#include "libgearman/protocol/submit.h"
-#include "libgearman/interface/client.hpp"
-#include "libgearman/interface/task.hpp"
-#include "libgearman/interface/worker.hpp"
-
-gearman_function_t gearman_function_create_null(void);
+gearman_return_t error_string2return_code(const char*, size_t length);
