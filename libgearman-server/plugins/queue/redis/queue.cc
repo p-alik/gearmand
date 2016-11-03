1fd1955fb2f1d23328540af38de7486ee07a9e87
Flavio Castelli <fcastelli@suse.com>
2016-11-03 14:20:19 +0100

    Redis queue: take server address into account
    The old code didn't consider the Redis server address, the code always
    attempted to connect to a Redis server running on localhost.
    
    Signed-off-by: Flavio Castelli <fcastelli@suse.com>

--- libgearman-server/plugins/queue/redis/queue.cc
+++ libgearman-server/plugins/queue/redis/queue.cc
@@ -116,7 +116,7 @@ Hiredis::~Hiredis()
 gearmand_error_t Hiredis::initialize()
 {
   int service_port= atoi(service.c_str());
-  if ((_redis= redisConnect("127.0.0.1", service_port)) == NULL)
+  if ((_redis= redisConnect(server.c_str(), service_port)) == NULL)
   {
     return gearmand_log_gerror(GEARMAN_DEFAULT_LOG_PARAM, GEARMAND_QUEUE_ERROR, "Could not connect to redis server: %s", _redis->errstr);
   }
