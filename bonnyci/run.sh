7310b818e91f760b61179310a3130e7f3c0f8a8a
Clint Byrum <clint@fewbar.com>
2017-03-21 09:41:52 -0700

    Fix bonnyci path and script

--- bonnyci/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eux
-sudo apt-get install -y build-essential libboost-all-dev gperf libevent-dev uuid-dev python-sphinx libhiredis-dev
-
-./bootstrap.sh -a
-./configure
-make
-make test
