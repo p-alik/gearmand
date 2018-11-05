f1d4c94fee78c7ef226493b93e04a12392d75e20
Alexei Pastuchov <info@maximka.de>
2018-11-05 14:34:17 +0100

    simplify CC and CXX setting as proposed in travis ci documentation
    https://docs.travis-ci.com/user/languages/cpp/#gcc-on-linux

--- scripts/travis-linux.sh
+++ scripts/travis-linux.sh
@@ -1,7 +1,3 @@
 sudo apt-get update -qq
-COMPILER_PACKAGE=""
-if [ "$COMPILER" = "g++-4.9" ]; then COMPILER_PACKAGE=g++-4.9
-elif [ "$COMPILER" = "g++-5" ]; then COMPILER_PACKAGE=g++-5; fi
+COMPILER_PACKAGE=$CXX
 sudo apt-get install -y libboost-all-dev gperf libevent-dev uuid-dev python-sphinx libhiredis-dev $COMPILER_PACKAGE
-if [ "$COMPILER" = "g++-4.9" ]; then export CXX="g++-4.9" CC="gcc-4.9"
-elif [ "$COMPILER" = "g++-5" ]; then export CXX="g++-5" CC="gcc-5"; fi
